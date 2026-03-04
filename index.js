aaa3975aa9f082c1aa283da09cc07b6a71dbdcbc
Abhishek Kumar <abhi3144.manu@gmail.com>
2026-03-04 14:22:23 +0530

    scrape till job search

--- index.js
+++ index.js
@@ -17,16 +17,43 @@ const getQoutes = async () => {
   await page.waitForSelector("input#usernameField");
   await page.waitForSelector("input#passwordField");
 
-  
-//await page.type('input#usernameField',process.env.USERNAME)
-//await page.type ('input#passwordField',process.env.PASSWORD)
+  await page.type("input#usernameField", "abhi3144.manu@gmail.com");
+  await page.type("input#passwordField", process.env.PASSWORD);
 
-await page.click(
-       "button[type='submit'].waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform",
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "networkidle0" }),
+    page.click(
+      "button[type='submit'].waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform",
+    ),
+  ]);
+
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "networkidle0" }),
+    page.click(
+      "li.nI-gNb-custom-Jobs.nI-gNb-menuItems a.nI-gNb-menuItems__anchorDropdown",
+    ),
+  ]);
+
+  //await page.waitForNavigation({ waitUntil: "networkidle2" });
+  await page.waitForSelector("article.jobTuple.bgWhite.z-depth-1");
+  
+  const jobs = await page.evaluate(() => {
+
+    let listofJobs = [];
+    const jobElements = document.querySelectorAll(
+      "article.jobTuple.bgWhite.z-depth-1",
     );
+        Array.from(jobElements).map( (job) => {
+           listofJobs.push({
+            title: job.querySelector("p").innerText,
+            company: job.querySelector("span.dspIB.valignM.subTitle.ellipsis.dspIB").innerHTML,
+        })
+    })
+    // const jobarticle =  jobElements[0].outerHTML;
+    console.log(listofJobs);
+  });
 
-
-  
+  console.log(jobs);
 };
 
 getQoutes();
